4d8af94dddeb5e2b3ed248a84e53c14bb5a62e34
Harutaka-Tsujino <glaca986@outlook.jp>
2018-09-13 11:23:57 +0900

    構造体追加
     -  ポリゴンの頂点情報を入れる構造体

--- DX9LibX64/DX9LibX64.h
+++ DX9LibX64/DX9LibX64.h
@@ -3,4 +3,14 @@
 
 #include<windows.h>
 
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ|D3DFVF_DIFFUSE)
+
+//ƒ|ƒŠƒSƒ“‚Ì’¸“_î•ñ‚Ì\‘¢‘Ì
+struct CustomVertex
+{
+	FLOAT m_x, m_y, m_z;
+	DWORD m_aRGB;
+	FLOAT m_tu, m_tv;
+};
+
 #endif//DX9_X64_LIB_H
